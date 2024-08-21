aaeff5746df67f2ca9cc666993363ebbae67a1d3
netia-melvin <melvinatemba18@gmail.com>
2024-08-21 11:15:16 +0000

    subs fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and returns
-the number of subscribers for a given subreddit.
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Queries to Reddit API """
-    u_agent = 'Mozilla/5.0'
-
-    headers = {
-        'User-Agent': u_agent
-    }
-
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    if response.status_code == 404:
         return 0
-    dict = response.json()
-    if 'data' not in dict:
-        return 0
-    if 'subscribers' not in dict.get('data'):
-        return 0
-    return response.json()['data']['subscribers']
+    results = response.json().get("data")
+    return results.get("subscribers")
